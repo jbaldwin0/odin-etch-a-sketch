bd6d7273cc6b928d5192ac9e7dc066539ff2f7fc
jbaldwin0 <jac0b67@ymail.com>
2022-07-31 17:01:36 -0500

    Create usuable formatted elements, begin dom scripting

--- js/script.js
+++ js/script.js
@@ -2,5 +2,12 @@ const container = document.querySelector('.container');
 
 for (let i = 0; i < 256; i++) {
     const content = document.createElement('div');
+    document.getElementById("content").id = "block";
     container.appendChild(content);
 }
+
+
+const hover = document.querySelector('block');
+hover.addEventListener('click', () => {
+    content.style.backgroundColor = "black";
+});
