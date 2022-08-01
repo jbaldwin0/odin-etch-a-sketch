aa25a63751e66c1f0138ff34208a4ca5b73f62d8
jbaldwin0 <jac0b67@ymail.com>
2022-07-31 19:43:45 -0500

    Setup partially working function

--- js/script.js
+++ js/script.js
@@ -2,12 +2,20 @@ const container = document.querySelector('.container');
 
 for (let i = 0; i < 256; i++) {
     const content = document.createElement('div');
-    document.getElementById("content").id = "block";
+    content.className = 'content';
     container.appendChild(content);
 }
 
+const blocks = Array.prototype.slice.apply(document.querySelectorAll('.content')
+);
 
-const hover = document.querySelector('block');
-hover.addEventListener('click', () => {
-    content.style.backgroundColor = "black";
-});
+for (content of blocks) {
+    content.addEventListener('mouseenter', () => {
+        content.style.backgroundColor = "black";
+      });
+      content.addEventListener('mouseleave', () => {
+        content.style.backgroundColor = "#b0c2c2";
+      });
+}
+
+
